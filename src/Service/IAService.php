84cf9c2c1aee5092e620615f8fee26eecab0f592
David Asunción <david.asuncion@flexoh.com>
2023-05-19 20:12:01 +0200

    Condiciones

--- src/Service/IAService.php
+++ src/Service/IAService.php
@@ -14,21 +14,34 @@ class IAService
         $this->openAi->setHeader(["Connection" => "keep-alive"]);
     }
 
+
     public function connect($question)
     {
         $chat = $this->openAi->chat([
             'model' => 'gpt-3.5-turbo',
             'messages' => [
                 [
+                    "role" => "system",
+                    "content" => "Actividades disponibles y sus btc:
+                    1. Caminar 5000 pasos (35 btc).
+                    2. Practicar un deporte (35 btc).
+                    3. Ir al gimnasio (35 btc).
+                    4. Beber 2 litros de agua (10 btc).
+                    5. Meditar 30 minutos (25 btc).
+                    6.Utilizar tarjeta (10 btc).
+                    7.Llamar a un familiar (5 btc).
+                    8.Comer saludable durante todo el dia (20 btc).
+                    9. 1 hora de formación / un curso (30 btc).
+                    "
+                ],
+                [
                     "role" => "user",
                     "content" => "$question"
                 ]
             ],
             'temperature' => 1.0,
-            'max_tokens' => 4000,
-            'frequency_penalty' => 0,
-            'presence_penalty' => 0,
         ]);
+        print($chat);
         // decode response
         $d = json_decode($chat);
         // Get Content
