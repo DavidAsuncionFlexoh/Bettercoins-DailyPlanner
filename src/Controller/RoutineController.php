79ee14a2a7802c5a94061fdcb78d4c1b83ccbe49
David Asunción <david.asuncion@flexoh.com>
2023-05-19 13:48:42 +0200

    IAService

--- src/Controller/RoutineController.php
+++ src/Controller/RoutineController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use App\Service\IAService;
 
 class RoutineController extends AbstractController
 {
@@ -15,7 +16,8 @@ class RoutineController extends AbstractController
     public function index(Request $request): JsonResponse
     {
         $post_data = json_decode($request->getContent(), true);
-
+        $srv = new IAService();
+        $srv->connect();
         $form = $post_data['RoutineForm'];
         print_r($form);
 
---
+++ src/Service/IAService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Service;
+
+use Orhanerday\OpenAi\OpenAi as OpenAi;
+
+class IAService
+{
+    public $openAi;
+    
+    public function __construct()
+    {
+        $this->openAi = new OpenAi('asd');
+    }
+    public function connect()
+    {
+
+        $chat = $this->openAi->chat([
+            'model' => 'gpt-3.5-turbo',
+            'messages' => [
+                [
+                    "role" => "system",
+                    "content" => "You are a helpful assistant."
+                ],
+                [
+                    "role" => "user",
+                    "content" => "Who won the world series in 2020?"
+                ],
+                [
+                    "role" => "assistant",
+                    "content" => "The Los Angeles Dodgers won the World Series in 2020."
+                ],
+                [
+                    "role" => "user",
+                    "content" => "Where was it played?"
+                ],
+            ],
+            'temperature' => 1.0,
+            'max_tokens' => 4000,
+            'frequency_penalty' => 0,
+            'presence_penalty' => 0,
+        ]);
+
+
+        var_dump($chat);
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
+        // decode response
+        $d = json_decode($chat);
+        // Get Content
+        echo ($d->choices[0]->message->content);
+    }
+}
